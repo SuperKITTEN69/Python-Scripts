10cb7c5f824adcdf2c136755d60ec189abe869ff
SuperKITTEN69 <112276428+SuperKITTEN69@users.noreply.github.com>
2025-03-14 17:19:55 +0000

    Update SimpleMathScript.py

--- SimpleMathScript.py
+++ SimpleMathScript.py
@@ -3,4 +3,6 @@ Dogs = 3    # You can Change the Value of Dogs to any number you want
 
 print("You have", Cats + Dogs, "Animals in Total")
 
+print("You may edit the values inside the script (you will need to open the script in a text editor)")
+
 a=input()
