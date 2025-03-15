aacc2fe5adcbd9b2d104b79d77e8aa5e513fd3b6
SuperKITTEN69 <112276428+SuperKITTEN69@users.noreply.github.com>
2025-03-15 10:34:41 +0000

    Add files via upload

---
+++ NameAgeJob.py
@@ -0,0 +1,7 @@
+Name = input("Whats your name? ")
+Age = input("How old are you? ")
+Job = input("What is your job? ")
+
+print(f"Your name is {Name}, you are {Age} years old and your job is {Job}")
+
+a=input() # This is just to keep the console open
