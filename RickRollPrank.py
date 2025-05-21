abaacb2275cf7e8b72b745a170f5f9a306154f86
SuperKITTEN69 <112276428+SuperKITTEN69@users.noreply.github.com>
2025-05-21 18:51:01 +0100

    Add files via upload

---
+++ RickRollPrank.py
@@ -0,0 +1,7 @@
+import webbrowser
+
+# Replace this with the YouTube URL you want to open
+youtube_url = "https://www.youtube.com/watch?v=xvFZjo5PgG0"
+
+# Open the URL in the default web browser
+webbrowser.open(youtube_url)
